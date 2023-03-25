1a7c8d4d376886fff950ca4faca14d27211c1e2d
sarahfadilah <124076822+sarahfadilah@users.noreply.github.com>
2023-03-25 19:22:34 +0700

    Add files via upload

---
+++ quiz2.php
@@ -0,0 +1,22 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sarah Fadilah S | D112121055</title>
+  </head>
+  <body>
+    <h1> Tugas Quiz 2</h1>
+    <?php
+    $jumlah =9;
+    for($i=1; $i<=$jumlah; $i++) {
+        $penampung ='' ;
+        for($z=$i; $z<=$jumlah; $z++){
+            $penampung .=$z;
+        }
+        echo $penampung. "<br>"; 
+        } 
+        echo 0; 
+        ?>
+  </body>
+</html>
